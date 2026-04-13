094f8959fbb79fb2511814495b5240f78df14d81
dcase <newemail@example.com>
2026-04-13 21:57:43 +0100

    include importance

--- models/random_forest.py
+++ models/random_forest.py
@@ -1,6 +1,6 @@
 from src.tabular_data_prep import tabular_data_generator
 from sklearn.ensemble import RandomForestClassifier
-
+from sklearn.inspection import permutation_importance
 
 
 features, labels = tabular_data_generator('tabulated_chords.csv')
@@ -16,3 +16,13 @@ rf = RandomForestClassifier(
 
 rf.fit(features, labels)
 print(f"OOB Accuracy: {rf.oob_score_}")
+
+# Calculate feature importance using permutation importance
+result = permutation_importance(rf, features, labels, n_repeats=10, random_state=42)
+importance_scores = result.importances_mean
+# Print feature importance scores
+for i, score in enumerate(importance_scores):
+    print(f"Feature {i}: Importance Score = {score:.4f}")   
+
+#Total of importance scores is 1
+# suggests root note may be less important, and final chord is more important?
